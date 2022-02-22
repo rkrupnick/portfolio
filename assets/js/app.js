0f8dc6e4c7a8e37c8d6b4320fed9d31811c70e8e
Rachel Krupnick <rachel@rachelkrupnick.com>
2022-02-22 13:43:52 -0500

    Update contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,4 @@
-// scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
 
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
 
 // Nav hamburger selections
 
